d858b1715275b455f040f902f92e71e2e08a41d2
Harshit Bhatt <043.harshitbhatt@gmail.com>
2023-07-30 19:45:06 +0530

    created sub sub-menu

--- menu.sh
+++ menu.sh
@@ -1,5 +1,39 @@
 #!/bin/bash
 
+ssm1() {
+    echo -ne "
+    UPDATE SYSTEM SUBMENU
+1 - UPDATE
+2 - UPGRADE
+3 - Go Back to SYSTEM SUBMENU
+4 - Go Back to MAIN MENU
+0 - Exit
+Choose an option:  "
+    read -r ans
+    case $ans in
+    1)
+        sudo apt update
+        ssm1
+        ;;
+    2)
+        sudo apt upgrade
+        ssm1
+        ;;
+    3)
+        sm1
+        ;;
+    4)
+        menu
+        ;;
+    0)
+        echo "Bye bye."; exit 0;
+        ;;
+    *)
+        echo "Wrong option,try again." exit 1;
+        ;;
+    esac
+}
+
 sm1() {
     echo -ne "
  SYSTEM SUBMENU
@@ -10,7 +44,7 @@ Choose an option:  "
     read -r ans
     case $ans in
     1)
-        echo "you have selected updates"
+        ssm1
         sm1
         ;;
     2)
@@ -24,7 +58,6 @@ Choose an option:  "
         ;;
     esac
 }
-
 
 menu() {
     echo -ne "

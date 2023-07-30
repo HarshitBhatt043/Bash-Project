826f2752890ec51c570ae17f17cf4d8b6755feac
Harshit Bhatt <043.harshitbhatt@gmail.com>
2023-07-30 19:36:53 +0530

    Left select and transitioned to menu and sub menu

--- menu.sh
+++ menu.sh
@@ -1,50 +1,51 @@
 #!/bin/bash
 
-submenu () {
-  local PS3='Please enter sub option: '
-  local options=("item 1" "item 2" "quit")
-  local opt
-  select opt in "${options[@]}"
-  do
-      case $opt in
-          "item 1")
-              echo "you chose $REPLY which is $options"
-              ;;
-          "item 2")
-              echo "you chose $REPLY which is $options"
-              ;;
-          "quit")
-              return
-              ;;
-          *) echo "invalid option $REPLY";;
-      esac
-  done
+sm1() {
+    echo -ne "
+ SYSTEM SUBMENU
+1 - UPDATES
+2 - Go Back to Main Menu
+0 - Exit
+Choose an option:  "
+    read -r ans
+    case $ans in
+    1)
+        echo "you have selected updates"
+        sm1
+        ;;
+    2)
+        menu
+        ;;
+    3)
+        echo "Bye bye."; exit 0;
+        ;;
+    *)
+        echo "Wrong option,try again." exit 1;
+        ;;
+    esac
 }
 
 
-PS3='Please enter main option: '
-options=("SYSTEM" "MANAGEMENT" "INSTALLATION" "BACKUP" "Submenu" "Quit")
-select opt in "${options[@]}"
-do
-    case $opt in
-        "SYSTEM")
-            echo "you chose $REPLY which is $opt"
-            ;;
-        "MANAGEMENT")
-            echo "you chose $REPLY which is $opt"
-            ;;
-        "INSTALLATION")
-            echo "you chose choice $REPLY which is $opt"
-            ;;
-        "BACKUP")
-            echo "you chose choice $REPLY which is $opt"
-            ;;      
-        "Submenu")
-            submenu
-            ;;      
-        "Quit")
-            exit
-            ;;
-        *) echo "invalid option $REPLY";;
+menu() {
+    echo -ne "
+    WELCOME TO MAIN MENU
+1 - SYSTEM
+0 - Exit
+Choose an option:  "
+    read -r ans
+    case $ans in
+    1)
+        sm1
+        menu
+        ;;
+
+    0)
+        echo "Bye bye."; exit 0;
+        ;;
+    *)
+        echo "Wrong option,try again." exit 1;
+        ;;
     esac
-done
+}
+
+menu
